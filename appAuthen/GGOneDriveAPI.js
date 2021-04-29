9f8caf404283ded06e56e2abd62accd810904e75
technical-g <technical.team@spheresoftsolutions.com>
2021-04-29 16:35:44 +0700

    this version included getuserbyEmail function but after test, the result : cannot access other user's info

--- appAuthen/GGOneDriveAPI.js
+++ appAuthen/GGOneDriveAPI.js
@@ -1,19 +1,72 @@
 
-function CallTestAPI(){
+//constant path for oneDrive API
+const apiPath = {
+    microsoftGraph : "https://graph.microsoft.com/v1.0",
+
+    searchKeywordTest : "/me/drive/root/search(q='test')?select=name,id,webUrl",
+    getUserInfoByEmail : "/users/"
+
+};
+
+
+function callTestAPI(){
+
     var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer EwCAA8l6BAAU6k7+XVQzkGyMv7VHB/h4cHbJYRAAAUEtMgVC5vo+MBNWIT0gztyZT3mDQuKdMLDdMJyiVoutooBDqGBrJc3UOT7Zm4JldTjtmjaeWtM8UKo330urTMq86R+X2uTVsPe3YSTzQqpEpq9mTiY8j8pnVZBkruw3Xlon5sgvUrIZVho2R8yRXLlrNkAXRh6CgYg7eW2SWYkfwkuVr1mJM67dmAIceYqKFTRmnsrSowihoY07gu2flKA52dMUuIBB1Poiv/maP27VdjfYVjHFmBygStDMqKb9WYOIlKVltMr2d/6xD3aq7iRLzpRVTrYeqt7cCcnDBuMgBxNLPBP5Gtd8fJFnKTAmNo2OU8Yut7EB3INB1SltpUcDZgAACKEt5uxTGpRIUALg/mQCLeRbDi2mdbC3DsBWjcPGiZvkz/fRr/JiAk2lvmmEPgQXzcb3iYNiyc5/dYOWRXeDB02eaeAHsYC4SCCIjrFI1YIgnACyxemY4coPy5o92bePZc9XjcafO+A9wTEAPtnb0ppmcZb9mb3tnJxs7Yvl8slEl11SqF2sCQ49heyxFHlb2s/lzlNp6ahm2h28dNwocxeMgve0G0D85VtIueqcZUDiAGdcf5bW6DsJzPidBvk3rjXrjuW5/jTxCIwKidQg9VosnLwBkAatSHsAmoa10Dtf0vgiHJRfMquHs6JPeb/rxmMOxnQwecTBBeNzE4ksPlOVenK+XEdKj73hJJZNolYp8XlGIdJnZRBYhe0XLbZPedscAWiH0Eqz9ALbgyF5ydpu2/iddDdSJR42YCgbc3dNOKcbOKWucID6GLtIvLQg42a70RNImivM8v7VRZQOMPB5mNqDgX0ejKFozJkRpvQdxN05MEo+3IbHfvXVZKnKrzX2qTE7p68N9vnZEBGOSGDIw5DU1TCugu4WDiJW1OBNJcvfGaA6WX+n9fjYjO1Rcb6K+vWV/s+CGTL+cjRawzhutZwHvsPWpNZEVbVEAxwEtRX04tdukMe9H2kTjX7A1UUMtVN8dlajOOSaBwi1s8A/1CB+1JI2JS9hCfWl4X45YqYeb8N9lFnCGWoySPz2kUmGbbrOpXJF0TWUKbuk9Jzl2wsouXlU9nzX6KQ6d3BYUeVFldID2i9eZJXGUI2gfQkTLkoMV4n4CLHw+BcVqdmAn1Hjguncfm1xnwI=");
 
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
+    getTokenRedirect(tokenRequest)
+      .then(response => {
+          
+          myHeaders.append("Authorization", "Bearer " + response.accessToken);
 
-    fetch("https://graph.microsoft.com/v1.0/me/drive/root/search(q='test')?select=name,id,webUrl", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+          var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+          };
+
+          fetch(apiPath.microsoftGraph + apiPath.searchKeywordTest, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+      }).catch(error => {
+        console.error(error);
+      });
+
+}  
+
+/*
+
+
+*/
+function getuserByEmail(){
+    var myHeaders = new Headers();
+    //get email from input of index.html
+    var email = document.getElementById("emailForSearch").value ;
+
+    getTokenRedirect(tokenRequest)
+      .then(response => {
+          
+          myHeaders.append("Authorization", "Bearer " + response.accessToken);
+
+          var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+          };
+
+          fetch(apiPath.microsoftGraph + apiPath.getUserInfoByEmail+email, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+    }).catch(error => {
+        console.error(error);
+    });
+
 }
+
+
 
 /*function calOneDriveAPI(pathAccess) {
 
